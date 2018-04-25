1bb469f51dec488b926456bcf9621bd9b7320f64
Paolo Mari Macaraig <pao.mari.macaraig@gmail.com>
2018-04-25 14:38:58 +0800

    Removed unnecessary projects

--- Tenderfoot/TenderfootPrayerForum/Library/_Database/_DB.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tenderfoot.Database;
-
-namespace TenderfootPrayerForum.Library._Database
-{
-    public class _DB : Schemas
-    {
-        public static Schema<Members> Members => CreateTable<Members>("members");
-    }
-}
--- Tenderfoot/TenderfootWebAPI/Library/_Database/_DB.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tenderfoot.Database;
-
-namespace TenderfootWebAPI.Library._Database
-{
-    public class _DB : Schemas
-    {
-        public static Schema<Members> Members => CreateTable<Members>("members");
-    }
-}
